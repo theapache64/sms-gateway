32236560bd84212c7c8dbe999b3463a6812224a1
theapache64-bravo <theapache64@gmail.com>
2016-11-28 21:28:15 +0530

    Added TestServlet

---
+++ api_server/src/com/theah64/sg/api_server/TestServlet.java
@@ -0,0 +1,19 @@
+package com.theah64.sg.api_server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by theapache64 on 28/11/16,9:23 PM.
+ */
+@WebServlet(urlPatterns = {"/test"})
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello World!");
+    }
+}
