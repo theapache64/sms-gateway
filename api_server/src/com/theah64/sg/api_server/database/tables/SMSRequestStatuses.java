9c354b4d08dade3446327d64434254d5cfad84a8
theapache64-bravo <theapache64@gmail.com>
2016-12-01 06:09:30 +0530

    Added new route '/add_statuses'

--- api_server/src/com/theah64/sg/api_server/database/tables/SMSRequestStatuses.java
+++ api_server/src/com/theah64/sg/api_server/database/tables/SMSRequestStatuses.java
@@ -1,14 +1,26 @@
 package com.theah64.sg.api_server.database.tables;
 
+import com.theah64.sg.api_server.database.Connection;
 import com.theah64.sg.api_server.models.SMSRequestStatus;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by theapache64 on 29/11/16,5:19 PM.
+ * recipient_id INT NOT NULL,
+ * status ENUM('SENT','DELIVERED','FAILED') NOT NULL DEFAULT 'SENT',
+ * occurred_at INT(11) NOT NULL ,
  */
 public class SMSRequestStatuses extends BaseTable<SMSRequestStatus> {
 
     private static final SMSRequestStatuses instance = new SMSRequestStatuses();
     public static final String COLUMN_RECIPIENT_ID = "recipient_id";
+    private static final String COLUMN_STATUS = "status";
+    private static final String COLUMN_OCCURRED_AT = "occurred_at";
 
     private SMSRequestStatuses() {
         super("sms_request_statuses");
@@ -16,5 +28,41 @@ public class SMSRequestStatuses extends BaseTable<SMSRequestStatus> {
 
     public static SMSRequestStatuses getInstance() {
         return instance;
+    }
+
+    public void add(final JSONArray jaStatuses) throws InsertFailedException, JSONException {
+
+        final String query = "INSERT INTO sms_request_statuses (recipient_id, status, occurred_at) VALUES (?,?,?);";
+        final java.sql.Connection con = Connection.getConnection();
+        try {
+            final PreparedStatement ps = con.prepareStatement(query);
+
+            for (int i = 0; i < jaStatuses.length(); i++) {
+                final JSONObject joStatus = jaStatuses.getJSONObject(i);
+
+                final String recId = joStatus.getString(COLUMN_RECIPIENT_ID);
+                final String status = joStatus.getString(COLUMN_STATUS);
+                final String occurredAt = joStatus.getString(COLUMN_OCCURRED_AT);
+
+                ps.setString(1, recId);
+                ps.setString(2, status);
+                ps.setString(3, occurredAt);
+
+                if (ps.executeUpdate() != 1) {
+                    throw new InsertFailedException("Failed to insert : " + joStatus);
+                }
+            }
+
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- api_server/src/com/theah64/sg/api_server/servlets/AddStatusesServlet.java
+++ api_server/src/com/theah64/sg/api_server/servlets/AddStatusesServlet.java
@@ -1,12 +1,15 @@
 package com.theah64.sg.api_server.servlets;
 
 import com.theah64.sg.api_server.database.tables.BaseTable;
-import com.theah64.sg.api_server.database.tables.Recipients;
+import com.theah64.sg.api_server.database.tables.SMSRequestStatuses;
+import com.theah64.sg.api_server.utils.APIResponse;
 import com.theah64.sg.api_server.utils.Request;
 import com.theah64.sg.api_server.utils.ServerHeaderSecurity;
+import org.json.JSONArray;
 import org.json.JSONException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,6 +18,7 @@ import java.io.IOException;
  * Created by shifar on 30/11/16.
  * server_key (as header), message_id, array( recepient_id,status, occurred_at)
  */
+@WebServlet(urlPatterns = {AdvancedBaseServlet.VERSION_CODE + "/add_statuses"})
 public class AddStatusesServlet extends AdvancedBaseServlet {
 
     private static final String KEY_STATUSES = "statuses";
@@ -31,15 +35,14 @@ public class AddStatusesServlet extends AdvancedBaseServlet {
 
     @Override
     protected String[] getRequiredParameters() {
-        return new String[]{Recipients.COLUMN_SMS_REQUEST_ID, KEY_STATUSES};
+        return new String[]{KEY_STATUSES};
     }
 
     @Override
     protected void doAdvancedPost() throws BaseTable.InsertFailedException, JSONException, BaseTable.UpdateFailedException, Request.RequestException {
-
-        final ServerHeaderSecurity serverHeaderSecurity = new ServerHeaderSecurity(getHttpServletRequest().getHeader(ServerHeaderSecurity.KEY_AUTHORIZATION));
-        final String serverId = serverHeaderSecurity.getServerId();
-
-
+        new ServerHeaderSecurity(getHttpServletRequest().getHeader(ServerHeaderSecurity.KEY_AUTHORIZATION));
+        final JSONArray jaStatuses = new JSONArray(getStringParameter(KEY_STATUSES));
+        SMSRequestStatuses.getInstance().add(jaStatuses);
+        getWriter().write(new APIResponse("Status(es) added", null).getResponse());
     }
 }
