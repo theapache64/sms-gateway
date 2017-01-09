0f93f125d4836330f58bb5d3a50fecff80a4a470
theapache64-alpha <theapache64@gmail.com>
2017-01-09 11:59:28 +0530

    Added SMSStatus model

--- client_server/app/src/main/java/com/theah64/smsgatewayserver/models/SMSStatus.java
+++ client_server/app/src/main/java/com/theah64/smsgatewayserver/models/SMSStatus.java
@@ -5,4 +5,34 @@ package com.theah64.smsgatewayserver.models;
  */
 
 public class SMSStatus {
+    private final String id,messageId, recipientId,status;
+    private final long occurredAt;
+
+    public SMSStatus(String id, String messageId, String recipientId, String status, long occurredAt) {
+        this.id = id;
+        this.messageId = messageId;
+        this.recipientId = recipientId;
+        this.status = status;
+        this.occurredAt = occurredAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public String getRecipientId() {
+        return recipientId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public long getOccurredAt() {
+        return occurredAt;
+    }
 }
